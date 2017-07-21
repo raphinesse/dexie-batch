9710aeb0ef670c41f5ac18709c199f12dda94139
Raphael von der Grün <raphinesse@gmail.com>
2017-07-21 02:09:55 +0200

    Add tests for serial operation

--- spec.js
+++ spec.js
@@ -5,29 +5,34 @@ const DexieBatch = require('./dexie-batch')
 const batchSize = 10
 const testEntries = Array(42).fill().map((_, i) => i)
 
-testWithTable('basic operation', (t, table) => {
+testWithTable('basic serial operation', (t, table) => {
+  return testBasicOperation(t, table, new DexieBatch({ batchSize }))
+})
+
+testWithTable('basic parallel operation', (t, table) => {
+  return table.count()
+    .then(n => new DexieBatch({ batchSize, limit: n }))
+    .then(db => testBasicOperation(t, table, db))
+})
+
+function testBasicOperation(t, table, db) {
   let maxIdx = -1
   const readEntries = []
 
-  return table.count()
-    .then(n => {
-      t.is(n, testEntries.length, 'written right number of entries')
-      return new DexieBatch({ batchSize, limit: n })
-    })
+  return Promise.resolve(db)
     .then(db => db.each(table.toCollection(), (entry, i) => {
       readEntries.push(entry)
       maxIdx = Math.max(maxIdx, i)
     }))
     .then(_ => {
       readEntries.sort((a, b) => a - b)
-      t.ok(maxIdx > 0, 'indices valid')
-      t.ok(maxIdx < batchSize, 'batches sized correctly')
+      t.equal(maxIdx + 1, batchSize, 'batches sized correctly')
       t.deepEqual(readEntries, testEntries, 'entries read correctly')
     })
-})
+}
 
 function testWithTable(name, f) {
-  const db = new Dexie('test-db')
+  const db = new Dexie(name)
   db.version(1).stores({ test: '++' })
   db.test.bulkAdd(testEntries)
     .then(_ => test(name, t => {
