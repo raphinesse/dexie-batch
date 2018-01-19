b070ecd8ed3e2487a44b6e5880a5ee557ab9bfa0
Raphael von der Grün <raphinesse@gmail.com>
2018-01-19 20:02:06 +0100

    Simplify tests

--- spec.js
+++ spec.js
@@ -5,44 +5,43 @@ const test = require('blue-tape')
 const Dexie = require('dexie')
 const DexieBatch = require('./dexie-batch')
 
+const numEntries = 42
 const batchSize = 10
-const testEntries = Array(42).fill().map((_, i) => i)
+const testEntries = Array.from(Array(numEntries), (_, i) => i)
 
-testWithTable('basic serial operation', (t, table) => {
-  return testBasicOperation(t, table, new DexieBatch({ batchSize }))
-})
+const serialBatchDriver = new DexieBatch({ batchSize })
+const parallelBatchDriver = new DexieBatch({ batchSize, limit: numEntries })
 
-testWithTable('basic parallel operation', (t, table) => {
-  return table.count()
-    .then(n => new DexieBatch({ batchSize, limit: n }))
-    .then(db => testBasicOperation(t, table, db))
-})
+testBasicOperation(serialBatchDriver)
+testBasicOperation(parallelBatchDriver)
 
-function testBasicOperation(t, table, db) {
-  let maxIdx = -1
-  const readEntries = []
+function testBasicOperation(batchDriver) {
+  const mode = batchDriver.isParallel() ? 'parallel' : 'serial'
+  testWithCollection(`basic ${mode} operation`, (t, collection) => {
+    let maxIdx = -1
+    const entries = []
 
-  return Promise.resolve(db)
-    .then(db => db.each(table.toCollection(), (entry, i) => {
-      readEntries.push(entry)
-      maxIdx = Math.max(maxIdx, i)
-    }))
-    .then(_ => {
-      readEntries.sort((a, b) => a - b)
-      t.equal(maxIdx + 1, batchSize, 'batches sized correctly')
-      t.deepEqual(readEntries, testEntries, 'entries read correctly')
-    })
+    return batchDriver
+      .each(collection, (entry, i) => {
+        entries.push(entry)
+        maxIdx = Math.max(maxIdx, i)
+      })
+      .then(_ => {
+        entries.sort((a, b) => a - b)
+        t.equal(maxIdx + 1, batchSize, 'batches sized correctly')
+        t.deepEqual(entries, testEntries, 'entries read correctly')
+      })
+  })
 }
 
-function testWithTable(name, f) {
+function testWithCollection(name, f) {
   test(name, t =>
-    Dexie.delete(name)
-      .then(_ => {
-        const db = new Dexie(name)
-        db.version(1).stores({ test: '++' })
-        return db.test.bulkAdd(testEntries)
-          .then(_ => db.test)
-      })
-      .then(table => f(t, table))
+    Dexie.delete(name).then(_ => {
+      const db = new Dexie(name)
+      db.version(1).stores({ test: '++' })
+      return db.test
+        .bulkAdd(testEntries)
+        .then(_ => f(t, db.test.toCollection()))
+    })
   )
 }
